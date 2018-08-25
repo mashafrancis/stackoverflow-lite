dcfe89983dfdf43bf5e4a639f2225395721bf19f
Francis Masha <francis@localhost.localdomain>
2018-08-25 15:29:02 +0300

    create user & question models

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'it is hard to guess'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
